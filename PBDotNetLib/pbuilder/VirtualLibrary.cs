ecc6bc8144ec9abd327781d63c1203865d1fe8c0
devbar <benjamin.kemner@gmx.de>
2015-05-01 12:47:16 +0200

    Added VirtualLibrary.cs

---
+++ PBDotNetLib/pbuilder/VirtualLibrary.cs
@@ -0,0 +1,43 @@
+﻿using PBDotNetLib.orca;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBDotNetLib.pbuilder {
+    public class VirtualLibrary : ILibrary {
+
+        private DirectoryInfo dirInfo = null;
+
+        public string Dir {
+            get { return dirInfo.FullName; }
+        }
+
+        public string File {
+            get { return dirInfo.Name; }
+        }
+
+        public orca.ILibEntry[] EntryList {
+            get {
+                var files = Directory
+                    .GetFiles(this.Dir, "*.*")
+                    .Where(f => f.ToLower().EndsWith(".psr") || f.Substring(f.Length -3, 2).ToLower() == "sr")
+                    .ToList();
+
+                var entries = new VirtualLibEntry[files.Count];
+
+                for (int i = 0; i < files.Count; i++) {
+                    entries[i] = new VirtualLibEntry(files[i]);
+                }
+
+                return entries;
+            }
+        }
+
+        public VirtualLibrary(string folder) {
+            this.dirInfo = new DirectoryInfo(folder);
+        }
+    }
+}
